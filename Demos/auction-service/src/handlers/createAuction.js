eae393c467150057f5d7c8588a5bcf498723cc37
jasonj85 <jasonj85@gmail.com>
2022-03-10 10:58:36 +0000

    updates for closing an auction automatically

--- Demos/auction-service/src/handlers/createAuction.js
+++ Demos/auction-service/src/handlers/createAuction.js
@@ -8,12 +8,15 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function createAuction(event, context) {
   const { title } = event.body;
   const now = new Date();
+  const endDate = new Date();
+  endDate.setHours(now.getHours() + 1); // end date of auction is in 1 hour
 
   const auction = {
     id: uuid(),
     title,
     status: "OPEN",
     createdAt: now.toISOString(),
+    endingAt: endDate.toISOString(),
     highestBid: {
       amount: 0,
     },
