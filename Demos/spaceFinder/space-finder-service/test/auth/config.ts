148ade6a95c4138e176804e64103e30afe301fb5
jasonj85 <jasonj85@gmail.com>
2022-03-29 08:32:56 +0100

    moving files to new folder structure

---
+++ Demos/spaceFinder/space-finder-service/test/auth/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  REGION: "eu-west-1",
+  USER_POOL_ID: "eu-west-1_s5VNUCJoO",
+  APP_CLIENT_ID: "n17ehr37h3h0mkljd7hmte35v",
+  IDENTITY_POOL_ID: "eu-west-1:4fba22ed-ae9a-459d-9984-b144e432f2b3",
+  TEST_USER_NAME: "devjay2",
+  TEST_USER_PASSWORD: "8ci5aY1@XkuOG&P3",
+};
--- Demos/spaceFinder/test/auth/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  REGION: "eu-west-1",
-  USER_POOL_ID: "xxx",
-  APP_CLIENT_ID: "xxx",
-  IDENTITY_POOL_ID: "xx",
-  TEST_USER_NAME: "xx",
-  TEST_USER_PASSWORD: "xx",
-};
